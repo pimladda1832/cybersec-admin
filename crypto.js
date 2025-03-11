8821fad1c3b11fae1389804e893dde26e5a7771e
พิมลัดดา สุริยศ <s6602041520152@Computers-Mac-mini-74.local>
2025-03-11 17:43:22 +0700

    update cryto.js

---
+++ crypto.js
@@ -0,0 +1,11 @@
+const crypto = require("crypto-js");
+
+const my_password= 'mypass';
+const my_key= 'lovemelovemycat'
+//encode
+const encode =crypto.AES.encrypt(my_password,my_key);
+console.log('encode',encode.toString())
+
+//decode
+const decode =crypto.AES.decrypt(encode.toString(),my_key);
+console.log('decode',decode.toString(crypto.enc.Utf8));
